e89359e7c1916d2a14e2fdd4265805fccea916ae
Shawn Potter <spotter@mail.greenriver.edu>
2021-02-04 15:14:43 -0500

    added variables to the index

--- index.php
+++ index.php
@@ -7,16 +7,30 @@
 
   //require autoload file
   require_once("vendor/autoload.php");
+  require_once("model/data-layer.php");
 
   //create an instance of the base class
   $f3 = Base::instance();
   $f3->set('DEBUG', 3);
 
-  //Define a default route (home page)
-  $f3->route('GET /', function(){
+  $f3->route('GET /', function($f3){
+    //save variables
+    $f3->set('username','jsmo');
+    $f3->set('password',sha1("Password01"));
+    $f3->set('title','Working with Templates');
+    $f3->set('ftemp', 67);
+    $f3->set('color', 'purple');
+    $f3->set('radius', 10);
+    $f3->set('fruits',array("apple","banana","orange"));
+
     $view = new Template();
     echo $view->render('views/info.html');
   });
+  //Define a default route (home page)
+  /*$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/info.html');
+  });*/
 
 
   //run fat free
