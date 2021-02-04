b622876fbb76ef25d4aae6105db23f4b511d10f8
Shawn Potter <spotter@mail.greenriver.edu>
2021-02-04 15:17:26 -0500

    added model folder and data-layer.php

---
+++ model/data-layer.php
@@ -0,0 +1,7 @@
+<?php
+// sdev328/week5/model/data-layer.php
+//define functions to "get data form somewhere
+function getFruit(){
+  $fruits = array('apple','banana','orange','kiwi');
+  return $fruits;
+}
